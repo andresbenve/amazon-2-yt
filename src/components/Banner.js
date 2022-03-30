6fd24b3586173988ce49b017b5e6bb937a5a2bd9
ANDRÉS BENVENUTO <56841920+andresbenve@users.noreply.github.com>
2022-03-30 01:03:42 -0300

    cambios para benve

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 function Banner() {
   return (
     <div className="relative">
-      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to transparent bottom-0 z-20" />
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-800 to transparent bottom-0 z-20" />
       <Carousel
         autoPlay
         infiniteLoop
@@ -17,21 +17,23 @@ function Banner() {
         <div>
           <img
             loading="lazy"
-            src="https://raw.githubusercontent.com/andresbenve/BancoDeImagenes/master/benveba1.png"
+            src="https://raw.githubusercontent.com/andresbenve/BancoDeImagenes/master/esculturaNegra.png"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            height="20"
+            width="20"
+            loading="lazy"
+            src="https://raw.githubusercontent.com/andresbenve/BancoDeImagenes/master/esculturaNegra.png"
             alt=""
           />
         </div>
         <div>
           <img
             loading="lazy"
-            src="https://raw.githubusercontent.com/andresbenve/BancoDeImagenes/master/benveba1.png"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            loading="lazy"
-            src="https://raw.githubusercontent.com/andresbenve/BancoDeImagenes/master/benveba1.png"
+            src="https://raw.githubusercontent.com/andresbenve/BancoDeImagenes/master/esculturaNegra.png"
             alt=""
           />
         </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,53 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import { useState } from "react";
-import { StarIcon } from "@heroicons/react/solid";
+
 //import Currency from "react-currency-formatter"; ----> import npm i react-currency-formatter
 
-const MIN_RATING = 1;
-const MAX_RATING = 5;
-
-function Products({ id, title, price, description, category, image }) {
-  const [rating] = useState(
-    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1) + MIN_RATING)
-  );
+function Products({ id, title, price, description, image }) {
   const [hasPrime] = useState(Math.random() < 0.5);
 
   return (
-    <div className="relative  flex flex-col m-5 bg-white z-30 p-10">
+    <div className="relative  flex flex-flex items-center  m-5 bg-white z-30 p-10">
       <div>
-        <p className="absolute top-2 right-2 text-xs italic text-gray-400 right-0">
-          {category}
-        </p>
-        <Image src={image} height={200} width={200} objectFit="contain" />
+        <Image src={image} height={350} width={300} objectFit="contain" />
         <h4 className="my-3">{title}</h4>
-        <div className="flex">
-          {Array(rating)
-            .fill()
-            .map((_, i) => (
-              <StarIcon className="h-5 " />
-            ))}
-        </div>
-        <p className="text-xs my-2 line-clamp-2">{description}</p>
         <div>
           {hasPrime && (
-            <div className="flex items-center space-x-2 -mt-5 pb-4 pt-4">
-              <img
-                className="w-12"
-                src="https://raw.githubusercontent.com/andresbenve/BancoDeImagenes/master/benveba1.png"
-                height={50}
-                width={50}
-                alt=""
-              />
-              <p>FREE Next-day Delivery</p>
+            <div className="flex items-center space-x-2 -mt-5 pb-1 pt-4">
+              <p>ON-Stock</p>
             </div>
           )}
+          <p className="pb-2">{price + "$"}</p>
         </div>
-        <button className="mt-auto button">Add To Basket</button>
+        <button className=" flex items-center mt-auto button">
+          Add To Basket
+        </button>
       </div>
     </div>
   );
 }
 //<Currency quantity={price} currency="GBP" />
-
+//  <p className="text-xs my-2 line-clamp-2">{description}</p>
 export default Products;
